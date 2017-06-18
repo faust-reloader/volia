5222178c91f226e816e546d7ed9bc0002198dfe3
fst <fst@ua.fm>
2017-06-18 19:58:16 +0300

    first commit

---
+++ src/app/app.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class HttpService{
+  constructor(private http: Http){ }
+  getData(){
+    return this.http.get('user.json');
+  }
+}
