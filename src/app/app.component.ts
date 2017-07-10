4dd8de846358203ccd5ac37dda5fa1815e8c6aa9
Smirnoff <smirnoffnew@gmail.com>
2017-07-10 11:42:28 +0300

    http andrew

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
-import {Title} from "@angular/platform-browser";
+import { Title } from "@angular/platform-browser";
+
+import { HttpService } from '/services/aseqwe';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 
+
 export class AppComponent {
-  onDeactivate() {
+  posts;
+  
+  constructor(public: httpService: HttpService) {
+  }
+  
+ ngOnInit() {
+  this.httpService.getData()
+  .subscribe(res => this.posts = res);
+} 
+  
+  
+
+
+
+onDeactivate() {
     document.body.scrollTop = 0;
   }
-  public constructor(private titleService: Title ) { }
+  //public constructor(private titleService: Title ) { }
   public setTitle( newTitle: string) {
     this.titleService.setTitle( newTitle );
   }
